2f233d7d7900902aed9c0ca04cf50129240255cb
marufbillah-dev <marufbillah.dev@gmail.com>
2026-04-14 00:40:06 +0600

    feat: restructure home layout by adding FriendsContainer and moving components to home directory

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import Hero from "@/components/layout/Hero";
-import MetricsSummary from "@/components/layout/MetricsSummary";
+import FriendsContainer from "@/components/layout/home/friends/FriendsContainer";
+import Hero from "@/components/layout/home/Hero";
+import MetricsSummary from "@/components/layout/home/MetricsSummary";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <MetricsSummary />
+      <FriendsContainer />
     </>
   );
 }
---
+++ src/components/layout/home/friends/FriendsContainer.jsx
@@ -0,0 +1,17 @@
+const FriendsContainer = () => {
+  return (
+    <section className="pt-10 pb-20">
+      <div className="container mx-auto px-4 sm:px-6">
+        {/* Heading */}
+        <h2 className="text-xl sm:text-2xl text-heading font-semibold mb-5">
+          Your Friends
+        </h2>
+
+        {/* Friends Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6"></div>
+      </div>
+    </section>
+  );
+};
+
+export default FriendsContainer;
